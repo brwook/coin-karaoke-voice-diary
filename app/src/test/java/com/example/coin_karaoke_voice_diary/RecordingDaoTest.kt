44098cc45c40e8ab069184ae1d6ffad5967b63ad
brwook <brwook@naver.com>
2025-05-19 06:05:56 +0900

    Add thumbnail path to recording

---
+++ app/src/test/java/com/example/coin_karaoke_voice_diary/RecordingDaoTest.kt
@@ -0,0 +1,47 @@
+package com.example.coin_karaoke_voice_diary
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.example.coin_karaoke_voice_diary.data.AppDatabase
+import com.example.coin_karaoke_voice_diary.data.Recording
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class RecordingDaoTest {
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = AppDatabase.createInMemory(context)
+    }
+
+    @After
+    fun teardown() {
+        db.close()
+    }
+
+    @Test
+    fun insert_and_query() = runBlocking {
+        val dao = db.recordingDao()
+        val recording = Recording(
+            filePath = "path/to/file.wav",
+            thumbnailPath = "path/to/thumbnail.jpg",
+            title = "Song Title",
+            artist = "Artist",
+            recordedAt = 1L
+        )
+        dao.insert(recording)
+
+        val all = dao.getAll()
+        assertEquals(1, all.size)
+        val item = all[0]
+        assertEquals("Song Title", item.title)
+        assertEquals("Artist", item.artist)
+        assertEquals("path/to/file.wav", item.filePath)
+        assertEquals("path/to/thumbnail.jpg", item.thumbnailPath)
+    }
+}
